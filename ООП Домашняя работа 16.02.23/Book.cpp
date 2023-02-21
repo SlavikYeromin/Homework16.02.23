96c5dcc8d6e6eee6f446f9d63dca6fd227106a78
Yeromin Slavik <slavyti42003@gmail.com>
2023-02-22 00:12:59 +0200

    Реализация класса Книга

--- ООП Домашняя работа 16.02.23/Book.cpp
+++ ООП Домашняя работа 16.02.23/Book.cpp
@@ -1,1 +1,29 @@
 #include "Book.h"
+
+Book::Book()
+{
+    rezyme = "Undefined";
+}
+
+Book::Book(string nazvanie, string author, int god_izdaniya, string rezyme)
+{
+    this->rezyme = rezyme;
+}
+
+void Book::setRezyme(string rezyme)
+{
+    this->rezyme = rezyme;
+}
+
+string Book::getRezyme() const
+{
+    return rezyme;
+}
+
+void Book::show() const
+{
+    cout << "Название: " << nazvanie << endl;
+    cout << "Автор: " << author << endl;
+    cout << "Год издания: " << god_izdaniya << endl;
+    cout << "Резюме: " << rezyme << endl;
+}
