551d1e579fc37870194d3e26eef346e8239009e2
Yeromin Slavik <slavyti42003@gmail.com>
2023-02-22 23:37:48 +0200

    Реализация класса EditionYearException

--- ООП Домашняя работа 16.02.23/EditionYearException.cpp
+++ ООП Домашняя работа 16.02.23/EditionYearException.cpp
@@ -1,1 +1,15 @@
 #include "EditionYearException.h"
+
+EditionYearException::EditionYearException()
+{
+}
+
+EditionYearException::EditionYearException(string name)
+{
+    this->name = name;
+}
+
+string EditionYearException::Message() const
+{
+    return "Невозможно создать издание - указан некорректный год издания";
+}
