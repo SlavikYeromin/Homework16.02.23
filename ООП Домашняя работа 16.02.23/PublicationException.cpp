5520f5c445507d8892107405f2dde9f6c693ae7e
Yeromin Slavik <slavyti42003@gmail.com>
2023-02-22 23:24:10 +0200

    Реализация класса PublicationException

--- ООП Домашняя работа 16.02.23/PublicationException.cpp
+++ ООП Домашняя работа 16.02.23/PublicationException.cpp
@@ -1,1 +1,11 @@
 #include "PublicationException.h"
+
+PublicationException::PublicationException()
+{
+	name = "Undefined";
+}
+
+PublicationException::PublicationException(string name)
+{
+	this->name = name;
+}
